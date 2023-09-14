2d8c26bebba1e816324aae9f531d285511635d6b
Harsh Keshari <134734274+Keshari818@users.noreply.github.com>
2023-09-15 00:42:28 +0530

    Time: 6 ms (61.27%), Space: 6.7 MB (42.74%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,20 +1,25 @@
-class Solution {
-public:
-    string multiply(string num1, string num2) {
-        int x = num1.size();
-        int y = num2.size();
-        string ans(x + y, '0');
-        for(int i=x-1;i>=0;i--){
-            for(int j = y - 1; j >= 0; j--){
-                int p = (num1[i] - '0') * (num2[j] - '0') + (ans[i + j +1] - '0');
-                ans[i+j+1] = p % 10 + '0';
-                ans[i+j] += p / 10 ;
+class Solution
+{
+    public:
+        string multiply(string num1, string num2)
+        {
+            int x = num1.size();
+            int y = num2.size();
+            string ans(x + y, '0');
+            for (int i = x - 1; i >= 0; i--)
+            {
+                for (int j = y - 1; j >= 0; j--)
+                {
+                    int p = (num1[i] - '0') *(num2[j] - '0') + (ans[i + j + 1] - '0');
+                    ans[i + j + 1] = p % 10 + '0';
+                    ans[i + j] += p / 10;
+                }
             }
+            for (int p = 0; p < x + y; p++)
+            {
+                if (ans[p] != '0')
+                    return ans.substr(p);
+            }
+            return "0";
         }
-        for(int p = 0; p < x+y; p++){
-            if(ans[p] !='0')
-                return ans.substr(p);
-        }
-        return "0";
-    }
 };
