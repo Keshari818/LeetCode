9d420c4595cc5370a302475ec2bafa1949c0c5bf
Harsh Keshari <134734274+Keshari818@users.noreply.github.com>
2024-03-16 17:00:49 +0530

    Time: 29 ms (42.64%), Space: 17.8 MB (47.32%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -2,20 +2,35 @@ class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         
-        int n = matrix.size();
-        int left = matrix[0][0], right = matrix[n - 1][n - 1];
+//         int n = matrix.size();
+//         int left = matrix[0][0], right = matrix[n - 1][n - 1];
         
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            int count = 0;
-            for (int i = 0; i < n; ++i) {
-                count+=upper_bound(matrix[i].begin(),matrix[i].end(),mid)-matrix[i].begin();
+//         while (left < right) {
+//             int mid = left + (right - left) / 2;
+//             int count = 0;
+//             for (int i = 0; i < n; ++i) {
+//                 count+=upper_bound(matrix[i].begin(),matrix[i].end(),mid)-matrix[i].begin();
+//             }
+//             cout<<count<<endl;
+//             if (count < k)
+//                 left = mid + 1;
+//             else
+//                 right = mid;
+//         }
+//         return left;
+        
+        
+        //Brute solution -------------------------------------------
+        
+        priority_queue<int>pq;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                pq.push(matrix[i][j]);
+                if(pq.size()>k)
+                    pq.pop();
             }
-            if (count < k)
-                left = mid + 1;
-            else
-                right = mid;
         }
-        return left;
+        return pq.top();
+        
     }
 };
