7063ab9349a5eea9eedf6e1e43ac41dc45e180fe
Harsh Keshari <134734274+Keshari818@users.noreply.github.com>
2024-03-19 02:04:03 +0530

    Time: 4 ms (65.75%), Space: 15.8 MB (49.43%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
-    static bool comp(int a,int b){
-        string aa = to_string(a);
-        string bb = to_string(b);
-        return aa+bb > bb+aa;
-    }
+    // static bool comp(int a,int b){
+    //     string aa = to_string(a);
+    //     string bb = to_string(b);
+    //     return aa+bb > bb+aa;
+    // }
     
-    string largestNumber(vector<int>& B) {
-        vector A = B;
-        int n=A.size();int ct=0;
-        for(int i=0;i<n;i++){
-            if(A[i]==0)
-            ct++;
+    string largestNumber(vector<int>& B) {    
+        // vector A = B;
+        // int n=A.size();
+        // int ct=0;
+        // for(int i=0;i<n;i++){
+        //     if(A[i]==0)
+        //     ct++;
+        // }
+        // if(ct==n)
+        //     return "0";
+        // sort(A.begin(),A.end(),comp);
+        // string s;
+        // for(int i=0;i<n;i++)
+        //     s.append(to_string(A[i]));
+        // return s;
+   
+
+
+ 
+        int n=B.size();
+        vector<string> strs;
+        for (int i = 0; i < B.size(); ++i) {
+            strs.push_back(to_string(B[i]));
         }
-        if(ct==n)
-            return "0";
-        sort(A.begin(),A.end(),comp);
-        string s;
-        for(int i=0;i<n;i++)
-            s.append(to_string(A[i]));
-        return s;
+        
+        sort(strs.begin(), strs.end(), [](const string& a, const string& b) {
+            return a + b > b + a;
+        });
+        
+        if (strs[0] == "0") return "0";
+        string res;
+        for(auto x:strs){
+            res+=x;
+        }
+        return res;
     }
 };
