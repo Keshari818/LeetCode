9d6740151e161f0fdfef2cb02ae738a28d984439
Harsh Keshari <134734274+Keshari818@users.noreply.github.com>
2024-04-15 11:19:27 +0530

    Time: 4 ms (66.60%), Space: 13.6 MB (10.04%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -13,22 +13,52 @@ public:
     ListNode* partition(ListNode* head, int x) {
         ListNode* dummy1 = new ListNode();
         ListNode* temp1 = dummy1;
-        ListNode* dummy2 = new ListNode();
-        ListNode* temp2 = dummy2;
-        while (head != NULL) {
-            if (head->val < x) {
-                temp1->next = head;
-                temp1 = temp1->next;
-            } else {
-                temp2->next = head;
-                temp2 = temp2->next;
+        
+        ListNode* temp2=head;
+        while(head){
+            if(head->val<x){
+                temp1->next=new ListNode(head->val);
+                temp1=temp1->next;
+                
+                
             }
-            head = head->next;
+            head=head->next;
+            
         }
-        if (dummy1->next == NULL)
-            return dummy2->next;
-        temp1->next = dummy2->next;
-        temp2->next = NULL;
+        while(temp2){
+            if(temp2->val>=x){   
+                temp1->next=new ListNode(temp2->val);
+                temp1=temp1->next;
+                
+                
+            }
+            temp2=temp2->next;
+            
+        }
         return dummy1->next;
+  
     }
 };
+        
+        
+        
+        
+        // ListNode* dummy2 = new ListNode();
+        // ListNode* temp2 = dummy2;
+        // while (head != NULL) {
+        //     if (head->val < x) {
+        //         temp1->next = head;
+        //         temp1 = temp1->next;
+        //     } else {
+        //         temp2->next = head;
+        //         temp2 = temp2->next;
+        //     }
+        //     head = head->next;
+        // }
+        // if (dummy1->next == NULL)
+        //     return dummy2->next;
+        // temp1->next = dummy2->next;
+        // temp2->next = NULL;
+        // return dummy1->next;
+//     }
+// };
