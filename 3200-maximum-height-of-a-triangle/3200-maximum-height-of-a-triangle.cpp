8ddd94b414a4e7baa36197ff0e437f2fe1484993
Harsh Keshari <134734274+Keshari818@users.noreply.github.com>
2024-07-01 09:07:48 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (37.50%) - LeetHub

---
+++ 3200-maximum-height-of-a-triangle/3200-maximum-height-of-a-triangle.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxHeightOfTriangle(array<int, 2> c) {
+        int ans = 0;
+        for (int i = 1; c[i & 1] >= i; ++i) {
+            c[i & 1] -= i;
+            ++ans;
+        }   
+        return ans;
+    }
+    int maxHeightOfTriangle(int red, int blue) {
+        return max(maxHeightOfTriangle({red, blue}),maxHeightOfTriangle({blue, red}));
+    }
+};
